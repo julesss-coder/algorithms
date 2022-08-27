bb434086fd7bfb9e5cd0dd15c638b9dfc2406bf9
Julia <julesss@posteo.de>
2022-08-27 14:11:38 +0200

    Thought through problem again with pen and paper, wrote and traced strategy, implemented it. Passed LeetCode tests.

--- Leetcode 3. Longest Substring Without Repeating Characters - 2nd Try.js
+++ Leetcode 3. Longest Substring Without Repeating Characters - 2nd Try.js
@@ -58,302 +58,119 @@ They would start a new search AFTER the longest string found so far. BUT the lon
 ======*/
 
 
-/* 
-STRATEGY 1
+// STRATEGY 1 - BRUTE FORCE (LEET CODE APPROACH 1  )
 
-longest = ''
-currentLongest = ''
-
-if s.length === 0 || s.length === 1
-  return s.length
-
-For each character in string:
-  If character does not occur in currentLongest:
-    currentLongest += character
-    
-    If currentLongest > longest:
-      longest = currentLongest
-  Else if character does occur in currentLongest:
-    currentLongest = ''
-    currentLongest += character
-  
-Return longest.length
-
-*/
-
-// IMPLEMENTATION STRATEGY 1  
-
-var lengthOfLongestSubstring = function(s) {
-  // longest = ''
-  let longest = '';
-  // currentLongest = ''
-  let currentLongest = '';
-
-  // if s.length === 0 || s.length === 1
-  if (s.length === 0 || s.length === 1) {
-    return s.length;
-  }
-  
-  // For each character in string:
-  for (let i = 0; i < s.length; i++) {
-    //   If character does not occur in currentLongest:
-    if (currentLongest.includes(s[i]) === false) {
-      currentLongest += s[i];
-      if (currentLongest.length > longest.length) {
-        longest = currentLongest;
-      }
-    //   Else if character does occur in currentLongest:
-    } else if (currentLongest.includes(s[i])) { 
-      currentLongest = '';
-      currentLongest += s[i];
-    }
-  }
-
-  return longest.length;   
-};
-
-// TRACE IMPLEMENTATION STRATEGY 1 
-
-/* 
-s = 'ksdfhfixyztf' // should be fixyzt
-                *
-
-longest = fixyzt
-currentLongest = f
-returns 6
-
-===
-
-s = pwwkew
-         *
-longest = wke
-currentLongest = w
-
-return 3
-
-*/
-
-// STRATEGY 2
-
-// if string length is 0 or 1:
-//   return string length
-
-// make a copy of string
+// Input: s = "pwwkew"
+// i               *
+// j               *
 
 // longest = ''
-// currentLongest = ''
+// substrings = []
 
-// For each character in stringCopy:
-//   If character does not occur in currentLongest:
-//     currentLongest += character
+// Get all substrings:
+//   For each char i in string:  
+//     For each char j in string, where j = i:
+//       Get substring from i to j (inclusive)
+//       Store substring in substrings
 
-//     If currentLongest > longest:
-//     longest = currentLongest
-//   Else if character occurs in currentLongest:
-//     If character !== left character:
-//       Find first occurrence of current character in string
-//       Cut off string from 0 to (including) first occurrence of current character
-//     Else if character === left character:
-//       Cut off substring `currentLongest` from string (i.e. cut off substring with length of currentLongest from beginning of string)
-      
-//     // start a new search at current letter:
-//     currentLongest = ''
-//     i = 0
-//     currentLongest += character
+// For each substring in substrings:
+//   // call allUnique(substring):*
+//   If it has no repeating characters:
+//     If it is longer than longest:
+//       longest = current substring
 
-// Return longest.length
-
-/* 
-
-// ===== TRACE STRATEGY 2 ======
-
-s = 'ksdfhfixyztff' 
-copy:
-s = 'f' 
-     *
-
-longest = hfixyzt
-current = f
-returns 7
+// return substring.length
 
 
-*/
 
-/* 
+// * Check if a substring has no repeating characters
+// function allUnique(substring):
+//   if substring.length is 0 or 1:
+//     if substring > longest:
+//       longest = substring
 
-IMPLEMENTATION STRATEGY 2
-*/
-var lengthOfLongestSubstring = function(s) {
-  // if string length is 0 or 1:
-  if (s.length === 0 || s.length === 1) {
-    return s.length;
-  }
-  
-  // make a copy of string
-  let sCopy = s.slice();
-  
-  let longest = '';
-  let currentLongest = '';
-  
-  // For each character in stringCopy:
-  for (let i = 0; i < sCopy.length; i++) {
-    //   If character does not occur in currentLongest:
-    if (currentLongest.includes(sCopy[i]) === false) {
-      currentLongest += sCopy[i];
-  
-      if (currentLongest.length > longest.length) {
-        longest = currentLongest;
-      }
-    } else {
-      if (sCopy[i] !== sCopy[i - 1]) {
-        // Find first occurrence of current character in string
-        let firstIndex = sCopy.indexOf(sCopy[i]);
-        // Cut off string from 0 to (including) first occurrence of current character
-        sCopy = sCopy.substring(firstIndex);
-      } else if (sCopy[i] === sCopy[i - 1]) {
-        // Cut off substring `currentLongest` from string (i.e. cut off substring with length of currentLongest from beginning of string)
-        sCopy = sCopy.substring(currentLongest.length);
-      }
-  
-      // start a new search at current letter:
-      currentLongest = '';
-      i = 0;
-      currentLongest += sCopy[i];
-    }
-  }
+//     return true
 
-  return longest.length;
-}
+//   charSet = new Set // or Map? Set, because Set only stores keys, no values, and we only have keys, no values
 
-// ===============
+//   For each char in substring:
+//     If char is not in charSet:
+//       Add it to charSet
+//     // If there are repeated characters in substring
+//     Else if char is in charSet:
+//       Empty charSet // Set.prototype.clear()
+//       Return false
 
-/* 
+//   Return true
 
-STRATEGY 3 - BRUTE FORCE (LEET CODE APPROACH 1  )
+// =======
+
+// TRACE STRATEGY 1
 
 // Input: s = "pwwkew"
-i               *
-j               *
+// i                 *
+// j                 *
 
-longest = ''
-substrings = []
+// longest = ''
+// substrings = 
+// p
+// pw
+// pww
+// pwwk
+// *
+// pwwke
+// pwwkew
+// w
+// ww
+// wwk
+// wwke
+// wwkew
+// w
+// wk
+// wke
+// *
+// wkew
+// k
+// ke
+// kew
+// e
+// ew
+// w
 
-Get all substrings:
-  For each char i in string:  
-    For each char j in string, where j = i:
-      Get substring from i to j (inclusive)
-      Store substring in substrings
+// charSet = {
+//   w
+//   k
+//   e
 
-For each substring in substrings:
-  // call allUnique(substring):*
-  If it has no repeating characters:
-    If it is longer than longest:
-      longest = current substring
-
-return substring.length
-
-----------
-"pwwkew"
- *
-* Check if a substring has no repeating characters
-function allUnique(substring):
-  if substring.length is 0 or 1:
-    if substring > longest:
-      longest = substring
-
-    return true
-
-  charSet = new Set // or Map? Set, because Set only stores keys, no values, and we only have keys, no values
-
-  For each char in substring:
-    If char is not in charSet:
-      Add it to charSet
-    // If there are repeated characters in substring
-    Else if char is in charSet:
-      Empty charSet // Set.prototype.clear()
-      Return false
-
-  Return true
-
-=======
-
-TRACE STRATEGY 3
-
-// Input: s = "pwwkew"
-i                 *
-j                 *
-
-longest = ''
-substrings = 
-p
-pw
-pww
-pwwk
-*
-pwwke
-pwwkew
-w
-ww
-wwk
-wwke
-wwkew
-w
-wk
-wke
-*
-wkew
-k
-ke
-kew
-e
-ew
-w
-
-charSet = {
-  w
-  k
-  e
-
-}
+// }
 
 
 
-*/
+// IMPLEMENTATION STRATEGY 1
 
-// IMPLEMENTATION STRATEGY 3
 // When I hit `run code`, it is run with 'abcabcbb', and the result is 'Accepted: Runtime: 87 ms'
 // When I hit 'Submit', I get a runtime error. Code takes too long with extremely long input.
-
-
 
 var lengthOfLongestSubstring = function(s) {
   let longest = '';
   let substrings = [];
 
   // Get all substrings:
-  //   For each char i in string:  
   for (let i = 0; i < s.length; i++) {
     for (let j = i; j < s.length; j++) {
       let substring = s.substring(i, j + 1);
       substrings.push(substring);
     }
   }
-  //     For each char j in string, where j = i:
-  //       Get substring from i to j (inclusive)
-  //       Store substring in substrings
 
-  // For each substring in substrings:
   substrings.forEach(substring => {
-    // If susbtring has only unique characters:
     if (allUnique(substring)) {
-      // set longest to the longest substring
       if (substring.length > longest.length) {
         longest = substring; // Actually, we only need the length and not the substring itself
       }
     }
   });
 
-  // console.log(longest);
   return longest.length;
 }
 
@@ -377,95 +194,55 @@ function allUnique(substring) {
 }
 
 
-
-
-
 /* 
-
-TRACE IMPLEMENTATION STRATEGY 3
-
-s = "abcabcbb"
-i    *
-j     *
-
-let longest = '';
-let substrings = [];
-a
-ab
- *
-abc
-*
-abca
-abcab
-abcabcb
-abcabcbb
-b
-bc
-bca
-bcab
-bcabcb
-bcabcbb
-
-*/
+STRATEGY 2 - SLIDING WINDOW APPROACH (LEETCODE APPROACH 2)
 
 
-// ==================
-
-/* 
-
-STRATEGY 4 (LEETCODE APPROACH 2 - SLIDING WINDOW)
-    012345678
-s = pwwkewafg
-i   *
-j   *
-
-longest = 0
-i = 0
-
+currentLongest = 0
+longest = 
 map = {}
+j = 0 // j = window start
 
 for each char in string:
-  if char is in map, within current window (i <= char <= j):
-    // contract the window on the left to start one position after the FIRST occurrence of the duplicate char
-    i = map[char] // index of first occurrence of current char
-    // Since we moved the window along, now update the duplicate letter in map to its current occurrence
-    map[char] = j
-  else if char is not in map:
-    add key-value pair char-index to map
-
-    // compare length of current substring to longest
-    if (j - i) + 1 > longest:
-      longest = (j - 1) + 1
-
-return longest
-
+  if char not in map: 
+    add char and char index to map
+    currentLongest++
+  else if char is in map:
+    if char index in map is within window (>= i, <= j):
+      // contract window from the left, until after index of duplicate char in map
+      j = (index of current char in map) + 1
+    else if char index in map is outside window:
+      currentLongest++
+  
+  update index of current char in map
+  if currentLongest > longest:
+    longest = currentLongest
 */
 
-// IMPLEMENTATION STRATEGY 4
-
+// IMPLEMENTATION STRATEGY 2
 var lengthOfLongestSubstring = function(s) {
+  let currentLongest = 0;
   let longest = 0;
-  let i = 0;
-  
   let map = {};
-  
-  for (let j = 0; j < s.length; j++) {
-    //   if char is in map, within current window (i <= char <= j):
-    if (map[s[j]] !== undefined && map[s[j]] >= i) {
-      // contract the window on the left to start one position AFTER the FIRST occurrence of the duplicate char
-      i = map[s[j]] + 1; // index of first occurrence of current char
-      // Since we moved the window along, now update the duplicate letter in map to its current occurrence
-      map[s[j]] = j;
-      //   else if char is not in map, or outside of current window:
-    } else if (map[s[j]] === undefined || map[s[j]] < i) {
-      //  add key-value pair char-index to map
-      map[s[j]] = j;
-      // compare length of current substring to longest
-      longest = Math.max(longest, ((j - i) + 1));
+  let j = 0; // window begins at j, ends at i
+
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] === undefined) {
+      currentLongest++;
+    } else {
+      // if current char is within window between j and i:
+      if (map[s[i]] >= j && map[s[i]] <= i) {
+        // contract window on left side until after previous occurence of duplicate character
+        j = map[s[i]] + 1;
+        currentLongest = i - j + 1;
+      } else {
+        currentLongest++;
+      }
     }
+
+    map[s[i]] = i;
+    longest = Math.max(currentLongest, longest);
   }
-  
+
   return longest;
-}
-
-
+};
