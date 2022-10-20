89eaed18153115f7a92b571aa719b90cb5db7ad5
Julia <67640854+julesss-coder@users.noreply.github.com>
2022-10-20 11:57:59 +0200

    Simplified swapping logic

--- InsertionSortFinal.java
+++ InsertionSortFinal.java
@@ -1,53 +1,38 @@
 /*
-* TEST CASES
-* === TESTED ===
-* positive and neg numbers
-    * numbers including 0 OK
-    * no 0 OK
-    * duplicates OK
-    * no duplicates OK
-
-* negative numbers
-    * duplicates OK
-    * no duplicates OK
-    *
-* positive numbers
-    * duplicates OK
-    * no duplicates OK
-
-* * one number only OK
-* * empty array OK
-* */
+* ================================================================================
+* To do:
+* - In each iteration over array, check if array is sorted. If true, return array.
+* ================================================================================
+ * */
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class InsertionSortV2 {
+public class InsertionSortV3 {
     public static void main(String[] args) {
         ArrayList<Integer> nums = new ArrayList<Integer>(
-                Arrays.asList(34, -394, 0, 45, 0, 23)
+                Arrays.asList(5, -5, 0, 5, 222, -63)
         );
 
         // As first number is considered sorted, start iterating from the second:
         for (int indexOfNumToBeSorted = 1; indexOfNumToBeSorted < nums.size(); indexOfNumToBeSorted++) {
             int numToBeSorted = nums.get(indexOfNumToBeSorted);
 
-            // Find out where to insert numToBeSorted by comparing numToBeSorted with left neighbour:
+            // Find out where to insert numToBeSorted by comparing numToBeSorted with left neighbour.
+            // As long as numToBeSorted < current number, copy current number to the right.
+            // After all numbers >= numToBeSorted have been pushed one position to the right, insert numToBeSorted at insertionIndex.
             int i = indexOfNumToBeSorted - 1;
             if (numToBeSorted < nums.get(i)) {
                 // Checking if numToBeSorted < nums[i] is necessary despite previous line, because `i` is changed.
                 while (i >= 0 && numToBeSorted < nums.get(i)) {
+                    // Copy current number:
+                    int temp = nums.get(i);
+                    // Insert current number to the right
+                    nums.set(i + 1, temp);
+                    //
                     i--;
                 }
                 int insertionIndex = i + 1;
-
-                // Insert numToBeSorted at insertionIndex:
-                    // Starting with numToBeSorted, swap each number with its left neighbour until before insertionIndex.
-                for (int j = indexOfNumToBeSorted; j > insertionIndex ; j--) {
-                    int temp = nums.get(j - 1);
-                    nums.set(j, temp);
-                }
-
                 nums.set(insertionIndex, numToBeSorted);
             }
         }
