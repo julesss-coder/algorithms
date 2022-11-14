a0f666157c1b4e7a67472a42acba19d564cbd71c
Julia <67640854+julesss-coder@users.noreply.github.com>
2022-11-14 15:19:35 +0100

    Added checks for array lengths 0 and 1

--- InsertionSortFinal.java
+++ InsertionSortFinal.java
@@ -1,18 +1,30 @@
-/*
-* ================================================================================
-* To do:
-* - In each iteration over array, check if array is sorted. If true, return array.
-* ================================================================================
- * */
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class InsertionSortV3 {
     public static void main(String[] args) {
         ArrayList<Integer> nums = new ArrayList<Integer>(
-                Arrays.asList(5, -5, 0, 5, 222, -63)
+                Arrays.asList(3, 2, 1)
         );
+
+        if (nums.size() == 1 || nums.size() == 1) {
+            System.out.println("array `nums`: " + nums);
+            return;
+        }
+
+        // If array is already sorted, return it as it is
+        boolean arrayIsSorted = true;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums.get(i) > nums.get(i + 1)) {
+                arrayIsSorted = false;
+                break;
+            }
+        }
+
+        if (arrayIsSorted) {
+            System.out.println(nums);
+            return;
+        }
 
         // As first number is considered sorted, start iterating from the second:
         for (int indexOfNumToBeSorted = 1; indexOfNumToBeSorted < nums.size(); indexOfNumToBeSorted++) {
@@ -22,6 +34,7 @@ public class InsertionSortV3 {
             // As long as numToBeSorted < current number, copy current number to the right.
             // After all numbers >= numToBeSorted have been pushed one position to the right, insert numToBeSorted at insertionIndex.
             int i = indexOfNumToBeSorted - 1;
+
             if (numToBeSorted < nums.get(i)) {
                 // Checking if numToBeSorted < nums[i] is necessary despite previous line, because `i` is changed.
                 while (i >= 0 && numToBeSorted < nums.get(i)) {
