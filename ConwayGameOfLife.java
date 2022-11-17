e3b2ecdd9b3fb81ab8038b54f90406de337b68fd
Julia <67640854+julesss-coder@users.noreply.github.com>
2022-11-17 10:49:10 +0100

    Added final version

--- ConwayGameOfLife.java
+++ ConwayGameOfLife.java
@@ -2,10 +2,6 @@
 TODO
 - [ ] Add option for user to cancel game (i.e. game runs indefinitely, until user cancels) - does that make sense?
 - [ ] Check if number entered by user is integer.
-
-BUGS
-- [ ] numberOfLiveCells does not correspond to the actual number of 1s in generated field
-
  */
 
 import java.util.Arrays;
@@ -15,15 +11,16 @@ import java.util.Scanner;
 public class ConwayGameOfLifeV2 {
     public static void main(String[] args) {
         int[] fieldSize = letUserChooseFieldSize();
-        int fieldLength = fieldSize[0];
-        int fieldWidth = fieldSize[1];
+        int fieldWidth = fieldSize[0];
+        int fieldHeight = fieldSize[1];
 
         // Set number of live cells to random number
         Random random = new Random();
-        int numberOfLiveCells = random.nextInt(0, (int) (fieldLength * fieldWidth) / 2);
+        // I want there to be at least 3 live cells in the initial field
+        int numberOfLiveCells = random.nextInt(3, (int) (fieldWidth * fieldHeight) / 2);
         System.out.println("numberOfLiveCells: " + numberOfLiveCells);
 
-        int[][] field = initializeArray(fieldLength, fieldWidth, numberOfLiveCells);
+        int[][] field = initializeArray(fieldWidth, fieldHeight, numberOfLiveCells);
 
         // Let user choose number of generations (rounds) to play game
         Scanner scanner = new Scanner(System.in);
@@ -34,8 +31,8 @@ public class ConwayGameOfLifeV2 {
         // Play game `generations` times:
         int counter = 0;
         while (counter < generations) {
-            int[][] nextGeneration = createNextGeneration(field, fieldLength, fieldWidth);
-            System.out.println("nextgeneration: ");
+            int[][] nextGeneration = createNextGeneration(field);
+            System.out.println("next generation: ");
             for (int i = 0; i < nextGeneration.length; i++) {
                 System.out.println(Arrays.toString(nextGeneration[i]));
             }
@@ -61,15 +58,14 @@ public class ConwayGameOfLifeV2 {
         return field;
     }
 
-    public static int[][] createNextGeneration(int[][] field, int fieldLength, int fieldWidth) {
+    public static int[][] createNextGeneration(int[][] field) {
         // Make a copy of field, without references
-        int[][] newField = new int[fieldLength][fieldWidth];
+        int[][] newField = new int[field.length][field[0].length];
         for (int i = 0; i < field.length; i++) {
             // Make a deep copy of each int array
             newField[i] = Arrays.copyOf(field[i], field[i].length);
         }
         // Now, `newField` can be changed without `field` changing - tested. (Arrays.copyOf() only seems to work with primitive types.)
-        System.out.println("newField: " + Arrays.deepToString(newField));
 
         // Check 8 surrounding cells and count live neighbours
         for (int i = 0; i < field.length; i++) {
@@ -119,6 +115,7 @@ public class ConwayGameOfLifeV2 {
         System.out.println("Please choose the field width (integers only).");
         fieldWidth = scanner.nextInt();
 
+        // Can I return 2 values?
         int[] fieldSize = {fieldLength, fieldWidth};
         return fieldSize;
     }
@@ -127,15 +124,12 @@ public class ConwayGameOfLifeV2 {
     public static int[][] randomlyMarkCellsLive(int[][] field, int numberOfLiveCells) {
         int cellCounter = 0;
         for (int j = 0; j < field.length; j++) {
-            for (int k = 0; k < field[j].length; k++) {
+            for (int k = 0; k < field[j].length && cellCounter < numberOfLiveCells; k++) {
                 Random random = new Random();
                 double randomNum = random.nextDouble(0, 1);
                 if (randomNum > 0.3) {
                     field[j][k] = 1;
                     cellCounter++;
-                    if (cellCounter == numberOfLiveCells) {
-                        return field;
-                    }
                 }
             }
         }
