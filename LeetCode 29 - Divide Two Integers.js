65ea509a463591fcaf4c3d9cf43e7a1042ee1349
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-01-20 19:30:48 +0100

    Added faster solution

--- LeetCode 29 - Divide Two Integers.js
+++ LeetCode 29 - Divide Two Integers.js
@@ -2,41 +2,9 @@
 LeetCode 29. Divide Two Integers
 URL: https://leetcode.com/problems/divide-two-integers/description/
 
-Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
-
-The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
-
-Return the quotient after dividing dividend by divisor.
-
-Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−2**31, 2**31 − 1]. For this problem, if the quotient is strictly greater than 2**31 - 1, then return 2**31 - 1, and if the quotient is strictly less than -2**31, then return -2**31.
-
- 
-
-Example 1:
-
-Input: dividend = 10, divisor = 3
-Output: 3
-Explanation: 10/3 = 3.33333.. which is truncated to 3.
-Example 2:
-
-Input: dividend = 7, divisor = -3
-Output: -2
-Explanation: 7/-3 = -2.33333.. which is truncated to -2.
- 
-
-Constraints:
-
--231 <= dividend, divisor <= 231 - 1
-divisor != 0
-
-========================
-
-### Edge cases
-divisor = 0 
-divisor = 1
-divisor = -1
-
 */
+
+/* ============ VERSION 1: SUBTRACTING DIVISOR REPEATEDLY========== */
 
 var divide = function(dividend, divisor) {
   let counter = 0;
@@ -47,6 +15,7 @@ var divide = function(dividend, divisor) {
   }
 
   // Deal with integers outside the 32-bit signed integer range
+  // The result of -2147483648 ÷ -1 would be 2147483648, which is outside the integer range (ranges until 2**31 - 1, i.e. 2147483647)
   if (dividend == -2147483648 && divisor == -1) {
     return 2147483647;
   }
@@ -77,3 +46,62 @@ var divide = function(dividend, divisor) {
 
   return counter;
 };
+
+
+/* ============ VERSION 2: SUBTRACTING MULTIPLE OF DIVISOR ========== */
+
+
+
+var divideTwoIntegers = function(dividend, divisor) {
+  if (divisor === 0) {
+    return;
+  }
+
+  // Deal with integers outside the 32-bit signed integer range
+  // The result of -2147483648 ÷ -1 would be 2147483648, which is outside the integer range (ranges until 2**31 - 1, i.e. 2147483647)
+  if (dividend == -2147483648 && divisor == -1) {
+    return 2147483647;
+  }
+
+  if (divisor === 1) {
+    return dividend;
+  } else if (divisor === -1) {
+    return -dividend;
+  }
+
+
+  let quotientIsNegative = false;
+
+  if (
+    dividend < 0 && divisor > 0 || 
+    dividend > 0 && divisor < 0) {
+    quotientIsNegative = true;
+  }
+
+
+  dividend = Math.abs(dividend);
+  divisor = Math.abs(divisor);
+  let quotient = 0;
+  let copiesOfDivisor = 0;
+  
+  while (dividend >= divisor) {
+    // Dividend fits into divisor at least once
+    copiesOfDivisor = 1;
+    let value = divisor;
+  
+    // If also divisor * 2 fits into the dividend:
+    while (value + value <= dividend) {
+      value += value;
+      copiesOfDivisor += copiesOfDivisor;
+    }
+  
+    quotient += copiesOfDivisor;
+    dividend -= value;
+  }
+  
+  if (quotientIsNegative) {
+    quotient = -quotient;
+  }
+
+  return quotient;
+}  
