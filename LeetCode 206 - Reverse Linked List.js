e86cd6616cb99319ec26ea0d712fc1562f4ea86b
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-01-30 22:09:48 +0100

    Added faster solution (strategy 2)

--- LeetCode 206 - Reverse Linked List.js
+++ LeetCode 206 - Reverse Linked List.js
@@ -50,8 +50,45 @@ CONS
 // Summary: Reverse references of given list
 
 
+// Creating my own list for testing
+// function ListNode(val, next) {
+//   this.val = (val===undefined ? 0 : val)
+//   this.next = (next===undefined ? null : next)
+// }
+
+// let list = new ListNode(1);
+// list.next = new ListNode(2);
+// list.next.next = new ListNode(3);
+// console.log(list);
+// head = list;
+
+
+var reverseList = function(head) {
+  let current = head;
+  let previous = null;
+
+  while (current !== null) {
+    // Get a second reference to next node
+    let tempNext = current.next;
+    // Direct the `next` pointer of current node backwards, to previous node
+    current.next = previous;
+    // Move `previous` pointer to the following node
+    previous = current;
+    // Move `current` pointer to following node
+    current = tempNext;
+  }
+
+  // Set head to (what was previously) last node
+  head = previous;
+  return head;
+};
+
+/*
+PROS
++ faster than strategy 1
+CONS
+- slightly harder to understand for the reader
+*/
 
 // STRATEGY 3
-// Summary: recursive solution
-
-// Sample LeetCode solution
+// Summary: Recursive solution
