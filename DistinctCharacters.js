84b9f8a24cc1b90d82b55972ca9b745f6ebca689
julesss-coder <julesss@posteo.de>
2024-05-25 18:36:54 +0200

    Correct version 1. Add time and space complexity for both versions.

--- DistinctCharacters.js
+++ DistinctCharacters.js
@@ -48,54 +48,108 @@ return distinctChars.length
 
 =========================
 */
+// VERSION 1 - better time complexity than version 2
 
 function distinctCharacterCount(string) {
-  if (string === "") return 0;
+  if (string === "") return 0; // if-statement: 1 instruction; I can only count 1 return statement in the function, so I count the one for the worst-case scenario (the last one)
 
-  const distinctChars = {};
-  let charCount = 0;
+  const distinctChars = {}; // 1 instruction
+  let charCount = 0; // 1 instruction
 
-  for (let i = 0; i < string.length; i++) {
-    let character = string[i];
-    if (!distinctChars[character]) { // undefined or null?
-      distinctChars[character] = character; // Don't assign it a falsy value, otherwise the check above will always return false 
+  for (let i = 0; i < string.length; i++) { // string.length times
+    let character = string[i]; // 1 instruction
+    // If character is not in distinctChars, add it.
+    if (!distinctChars[character]) { // 1 instruction
+      distinctChars[character] = character; // 1 instruction
+      // It does not matter which value is assigned to distinctChars[character], as all strings will be forced to a truthy value in the above if condition. 
     }
   }
 
-  for (let string in distinctChars) {
-    charCount++;
+  for (let char in distinctChars) { // distinctChars-size times
+    charCount++;// 1 instruction
   }
 
-  return charCount;
+  return charCount;// 1 instruction
 }
+
+/* Time complexity:
+Counting the number of instructions:
+1 + 1 + 1 + n * (1 + 1 + 1) + n * 1 + 1
+= 3 + 3n + n + 1
+= 4 + 4n
+=> drop lower term (4) and coefficient (4 in 4n)
+=> time complexity is O(n)
+--------------------------
+
+Space Complexity:
+Counting memory units:
+1 for the function call
+1 for string (a memory address)
+1 for distinctChars
+1 for charCount
+1 for i
+1 for character
+string.length for each property added to distinctChars (in worst-case scenario)
+1 for char
+= 7 + n (string.length)
+=> drop lower term
+=> Space complexity: O(n)
+--------------------------
+
+*/
 // ===================================
+// VERSION 2 (worse time complexity than version 1)
 
-function distinctCharacterCountV2(string) {
-  if (string === "") return 0;
+// function distinctCharacterCountV2(string) {
+//   if (string === "") return 0;  // 1
 
-  const distinctChars = [];
-  let foundChar = false;
+//   const distinctChars = []; //1
+//   let foundChar = false; //1
 
-  for (let i = 0; i < string.length; i++) {
-    let character = string[i];
-    
-    // Check if character is in `distinctChars`
-    for (let j = 0; j < distinctChars.length; j++) {
-      if (distinctChars[j] === character) {
-        foundChar = true;
-        break;
-      }
-    } 
+//   for (let i = 0; i < string.length; i++) { // string.length times
+//     let character = string[i]; // 1
 
-    if (foundChar === false) {
-      distinctChars[distinctChars.length] = character;
-    } else {
-      foundChar = false;
-    }
-  }
+//     // Check if character is in `distinctChars`
+//     for (let j = 0; j < distinctChars.length; j++) { // distinctChar.length times
+//       if (distinctChars[j] === character) { // 1
+//         foundChar = true; // 1
+//         break; // I don't count this, as I am looking at the worst case scenario, where the last character in distinctChars is equal to character.
+//       }
+//     }
 
-  return distinctChars.length;
-}
+//     if (foundChar === false) { // 1
+//       distinctChars[distinctChars.length] = character; // 1
+//     } else { // not counting this, as it is the same amount of instructions as the first condition, and will not run if the first one runs
+//       foundChar = false;
+//     }
+//   }
+
+//   return distinctChars.length; // 1
+// }
+
+/*
+Time complexity: 
+1 + 1 + 1 + n * (1 + (n * (1 + 1) + 1 + 1) + 1
+= 4 + n * (3 + 2n)
+= 4 + 3n + 2n^2
+=> drop all but biggest term
+=> Time complexity: O(n^2)
+--------------------------
+
+Space complexity:
+Counting memory units:
+1 for string
+1 for distinctChars
+1 for foundChar
+1 for 1
+string.length, 
+  - for each string created
+  - for each j
+  - for each property added to distinctChars
+
+4 + n * 3
+=> Space complexity: O(n)
+*/
 
 
 /*
