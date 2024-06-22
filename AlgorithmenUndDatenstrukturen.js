ab982f7465cdf854695491008afd6a6d8ff3f81b
julesss-coder <julesss@posteo.de>
2024-06-22 14:37:05 +0200

    Add algorithm for multiplication

--- AlgorithmenUndDatenstrukturen.js
+++ AlgorithmenUndDatenstrukturen.js
@@ -24,6 +24,26 @@ return sumDigits as number
 ============
 
 1.2 Multiplikation: Die Schulmethode
+
+Multiplikation
+---------
+multiply n-digit number a with 1-digit number b. Base 10.
+
+carry = 0
+result = ''
+
+for each digit in a, starting from the right:
+  product = digitInA * b + carry * base
+  rightDigitOfProduct = product modulo 10
+  carry = Math.floor(product / 10)
+
+  add rightDigitOfProduct to beginning of result
+  If you are at the first (leftmost) digit in a, add carry to beginning of result
+
+return result
+
+-----
+
 Aufgabe 1.1.: Geben Sie ein Programm für die Multiplikation von a und b[j] an, das nur eine Phase hat.
 
 Erklärung der Notation: a[n-1] ... a[0] sind die Ziffern einer n-ziffrigen Zahl. Diese Zahl wird mit der einstelligen Zahl b[j] multipliziert. 
